3a550acfaa8a17e528afe1c342bae1f86f40205c
Berke <berkeonver@icloud.com>
2025-04-27 22:05:15 +0300

    file read added

--- src/lox/Lox.java
+++ src/lox/Lox.java
@@ -3,11 +3,23 @@ package lox;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
     public static void main(String[] args) throws IOException {
-        runPrompt();
+        if(args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
     }
     
     private static void runPrompt() throws  IOException {
